69c006f73515e7f33928986ec860bd075aa3ef2a
songzeng2016 <songzeng62@gmail.com>
2017-12-05 17:56:17 +0800

    添加样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,7 @@ const { wc } = app
 
 Page({
   data: {
+    weeks: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
   },
   onLoad: function () {
     const that = this
@@ -18,8 +19,12 @@ Page({
         }
         wc.get('forecast', getData, (res) => {
           console.log(res)
+          let futureList = res.data.HeWeather6[0].daily_forecast
+          for (let i in futureList) {
+            futureList[i].day = new Date(futureList[i].date).getDay()
+          }
           that.setData({
-            futureList: res.data.HeWeather6[0].daily_forecast
+            futureList
           })
         })
       },
